4da3e2b49cbc59aa2981f2f4554d17c8e1a6ee91
Sanyaur <gs.ujvari@gmail.com>
2022-01-25 15:55:35 +0100

    structure added

--- src/components/Expenses/NewExpense/ExpenseForm.js
+++ src/components/Expenses/NewExpense/ExpenseForm.js
@@ -2,7 +2,24 @@ import React from "react";
 import "./ExpenseForm.css";
 
 const ExpenseForm = () => {
-  return <form></form>;
+  return (
+    <form>
+      <div className='new-expense__controls'>
+        <div className='new-expense__control'>
+          <label>Title</label>
+          <input type='text' />
+        </div>
+        <div className='new-expense__control'>
+          <label>Amount</label>
+          <input type='number' min='0.01' step='0.01' />
+        </div>
+        <div className='new-expense__control'>
+          <label>Date</label>
+          <input type='text' min='2019-01-01' max='2030-12-31' />
+        </div>
+      </div>
+    </form>
+  );
 };
 
 export default ExpenseForm;
