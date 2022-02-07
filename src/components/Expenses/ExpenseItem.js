3bc684b37da2b07d556fd7dbf20f6ae35e737c67
Sanyaur <gs.ujvari@gmail.com>
2022-02-07 18:55:42 +0100

    test button commented out to make the Add Expense button work

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -4,17 +4,17 @@ import "./ExpenseItem.css";
 import Card from "../UI/Card";
 
 export default function ExpenseItem(props) {
-  const [title, setTitle] = useState(props.title);
+  // const [title, setTitle] = useState(props.title);
 
-  const clickHandler = () => setTitle("Updated!");
+  // const clickHandler = () => setTitle("Updated!");
 
   return (
     <Card className='expense-item'>
       <ExpenseDate date={props.date} />
       <div className='expense-item__description'>
-        <h2>{title}</h2>
+        <h2>{props.title}</h2>
         <div className='expense-item__price'>${props.amount}</div>
-        <button onClick={clickHandler}>Change Title</button>
+        {/* <button onClick={clickHandler}>Change Title</button> */}
       </div>
     </Card>
   );
