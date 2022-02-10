6d9995ad9f554ecf902e1cc916fa210b62d57ca7
Sanyaur <gs.ujvari@gmail.com>
2022-02-10 14:07:30 +0100

    refactoring dynamic expression

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import ExpenseItem from "./ExpenseItem";
+import ExpensesList from "./NewExpense/ExpensesList";
 import Card from "../UI/Card";
 import ExpensesFilter from "./NewExpense/ExpensesFilter";
 import "./Expenses.css";
@@ -15,26 +15,16 @@ const Expenses = (props) => {
     (item) => item.date.getFullYear().toString() === filteredYear
   );
 
-  let expenseContent = <p>No expenses found.</p>;
-
-  if (filteredExpenses.length > 0) {
-    expenseContent = filteredExpenses.map((expense) => (
-      <ExpenseItem
-        key={expense.id}
-        title={expense.title}
-        amount={expense.amount}
-        date={expense.date}
-      />
-    ));
-  }
   return (
-    <Card className='expenses'>
-      <ExpensesFilter
-        selected={filteredYear}
-        onChangeFilter={filterChangeHandler}
-      />
-      {expenseContent}
-    </Card>
+    <li>
+      <Card className='expenses'>
+        <ExpensesFilter
+          selected={filteredYear}
+          onChangeFilter={filterChangeHandler}
+        />
+        <ExpensesList items={filteredExpenses} />
+      </Card>
+    </li>
   );
 };
 
