7c637a31178b667cb407bb051ea11ba8b79aee81
Sanyaur <gs.ujvari@gmail.com>
2022-02-16 20:31:47 +0100

    added fun unique ID generator for keys

--- src/components/Chart/Chart.js
+++ src/components/Chart/Chart.js
@@ -2,6 +2,9 @@ import React from "react";
 import ChartBar from "./ChartBar";
 import "./Chart.css";
 
+// fun unique ID generator for list rendering 'key' property
+import { v4 as uuidv4 } from "uuid";
+
 const Chart = (props) => {
   const dataPointValues = props.dataPoints.map((dataPoint) => dataPoint.value);
   const totalMaximum = Math.max(...dataPointValues);
@@ -12,7 +15,8 @@ const Chart = (props) => {
       - for every datapoint, we render one ChartBar */}
       {props.dataPoints.map((dataPoint) => (
         <ChartBar
-          key={dataPoint.label}
+          // calling the 3rd party library to make unique keys
+          key={uuidv4()}
           value={dataPoint.value}
           maxValue={totalMaximum}
           label={dataPoint.label}
